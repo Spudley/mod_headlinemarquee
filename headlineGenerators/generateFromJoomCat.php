2fa520f7fe99ccda84cf444988187ec0f3f46be6
Simon Champion <simonc@sfwltd.co.uk>
2018-04-17 16:40:27 +0100

    Rework using Joomla's category model so that it correctly filters for published state, permissions, etc.

--- headlineGenerators/generateFromJoomCat.php
+++ headlineGenerators/generateFromJoomCat.php
@@ -9,26 +9,38 @@ defined('_JEXEC') or die;
 
 require_once('generateFromNone.php');
 require_once(JPATH_SITE."/components/com_content/models/category.php");
-require_once(JPATH_SITE."/components/com_content/helpers/route.php");
 
 class generateFromJoomCat extends generateFromNone
 {
     /**
-     * @todo: Doesn't take permissions, etc into account; just lists all articles straight from the DB.
+     * @todo: Add config for including subcats, only featured, sort order, etc.
      */
     public function getHeadlines()
     {
         $cat = (int)$this->params['headlines']->joomCat;
+        $limit = (int)$this->params['numberOfHeadlines'];
 
-        $db = JFactory::getDbo();
-        $query = $db->getQuery(true);
-        $query->select('*');
-        $query->from('#__content');
-        $query->where('catid="'.$cat.'"');
+        $model = JModelLegacy::getInstance('Articles', 'ContentModel', array('ignore_request' => true));
+        $model->getState();
 
-        $db->setQuery((string)$query);
-        $items = $db->loadObjectList();
- 
+        //Set app parameters in model
+        $app = JFactory::getApplication();
+        $appParams = $app->getParams();
+        $model->setState('params', $appParams);
+
+        //Set the filters based on the module params
+        $model->setState('list.start', 0);
+        $model->setState('list.limit', $limit);
+        $model->setState('filter.category_id', $cat);
+        $model->setState('filter.published', 1);
+
+        //User permissions
+        $access = !JComponentHelper::getParams('com_content')->get('show_noauth');
+        $authorised = JAccess::getAuthorisedViewLevels(JFactory::getUser()->get('id'));
+        $model->setState('filter.access', $access);
+
+        $items = $model->getItems();
+
         $output = [];
         foreach ($items as $item) {
             $output[] = [$item->title, JRoute::_(ContentHelperRoute::getArticleRoute($item->id, $cat))];
